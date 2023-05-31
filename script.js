6ae44887ac412ff249ed069c3af19a6e94bddab5
JasonBMa <JasonBMa@users.noreply.github.com>
2023-05-31 16:12:36 -0700

    Some css and javascript

--- script.js
+++ script.js
@@ -6,20 +6,26 @@ ClickMe.addEventListener("click", () => {
     alert("Your computer has been compromised!")
 })
 
-const duplicatorButton = document.getElementById("button2")
+const randomNumberButton = document.getElementById("button2")
 const numberDisplay = document.getElementById("numberDisplay")
 
-duplicatorButton.addEventListener("click", () => {
+randomNumberButton.addEventListener("click", () => {
     numberDisplay.textContent = Math.floor(Math.random()*99)
 })
-
-
 const RedirectButton = document.getElementById("button3")
+
+const duplicateButton = document.getElementById("DuplicateButton")
+const dotContainer = document.getElementById("dotContainer")
+duplicateButton.addEventListener("click", () => {
+    const newDot = document.createElement("div")
+    newDot.classList.add("dot");
+    dotContainer.appendChild(newDot);
+})
 
 let lastkey;
 document.addEventListener("keypress", (event) => {
     console.log(event);
-    if(lastkey == 'KeyR' && event.code == 'KeyA'){
+    if(lastkey === 'KeyR' && event.code === 'KeyA'){
         window.open("https://www.youtube.com/channel/UCTsLNOp1BUp8gAX9Ru3rpgQ","_blank")
     }
     lastkey = event.code;
