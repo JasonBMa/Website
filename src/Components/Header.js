cfcbc1582524d4407efde8f24e7e703fe83fe983
JasonBMa <JasonBMa@users.noreply.github.com>
2023-12-30 07:38:15 -0800

    barebones, started react project, refactoring from html+bootstrap into react+tailwind since I justgot some practice

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-full h-screen py-12 px-72 bg-slate-200 flex items-start">
+        <div className="w-2/3 text-6xl text-start">
+            <p className="font-bold">Hi, I'm Jason</p>
+            <p>Programmer</p>
+            <p className="text-4xl text-slate-500">Studying Computer Science @ UC Davis</p>
+        </div>
+        <div className="">
+            <img src="" alt="headerImg"></img>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
