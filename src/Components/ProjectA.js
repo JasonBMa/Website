c0a1de78d262193b029c2c4613d8e369e1471a1f
JasonBMa <JasonBMa@users.noreply.github.com>
2024-01-01 06:57:49 -0800

    github styling

--- src/Components/ProjectA.js
+++ src/Components/ProjectA.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 const ProjectA = ({title,body,image,gitHubLink,site}) => {
   return (
-    <div className="w-full h-96 py-10 px-40 flex">
+    <div className="w-full h-96 py-10 px-80 flex">
       <div className="flex flex-col text-left w-2/3">
-        <p className="text-5xl"> {title} </p>
-        <p className="text-xl"> {body} </p>
-        <a href={gitHubLink} target="_blank" alt="">Github</a>
+        <p className="text-6xl font-bold"> {title} </p>
+        <p className="text-2xl"> {body} </p>
+        <div className="h-1/3">
+          <a href={gitHubLink} className="" target="_blank" alt="">
+            <button class="border p-2 text-lg text-white font-bold rounded-2xl bg-slate-800">
+              Github
+            </button>
+          </a>
+        </div>
       </div>
-      <div className="w-1/3 h-40">
-        <a href={site} target="_blank"><img className="object-cover rounded-3xl drop-shadow-lg" src={require(`./Graphics/${image}`)} alt="" /></a>
+      <div className="w-1/2 h-40">
+        <a href={site} target="_blank"><img className="object-cover rounded-3xl drop-shadow-lg border" src={require(`./Graphics/${image}`)} alt="" /></a>
       </div>
     </div>
   )
