7e564f40e41b3562016b2f2fb8107125cdefa9b7
JasonBMa <JasonBMa@users.noreply.github.com>
2024-04-10 01:33:53 -0700

    Created and added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header';
 import Navbar from './Components/Navbar';
 import ProjectsList from './Components/ProjectsList';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Header/>
       <ProjectsList/>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import github from "./Graphics/Socials/Github_icon.png"
+import linkedin from "./Graphics/Socials/Black_Linkedin_icon.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex-col justify-center items-center w-full h-24 pt-6 bg-gray-800 text-white">
+      <div className="flex justify-around">
+        <a href="https://github.com/JasonBMa" target="_blank" rel="noreferrer">
+          <img src={github} alt="github" className="w-8 h-8 invert"/>
+        </a>
+        <a href="https://www.linkedin.com/in/jason-b-ma/" target="_blank" rel="noreferrer">
+          <img src={linkedin} alt="linkedin" className="w-8 h-8 invert"/>
+        </a>
+      </div>
+      <div className="h-1/2">
+        <p>© 2023 - <a href="https://www.linkedin.com/in/jason-b-ma/" target="_blank" rel="noreferrer">Jason Ma</a></p>
+
+      </div>
+
+    </div>
   )
 }
 
