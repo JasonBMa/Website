be8633277f163b7f710900fa6f6bf73ca4bb1369
JasonBMa <JasonBMa@users.noreply.github.com>
2024-04-18 23:58:23 -0700

    added new project, refactored the media project component to be more modular

--- src/Components/ProjectA.js
+++ src/Components/ProjectA.js
@@ -1,17 +1,62 @@
 import React from 'react'
 
-const ProjectA = ({ title, body, image, gitHubLink, site, collab, techUsed, video }) => {
+function renderSwitch(mediaType, media, site){
+  switch(mediaType) {
+    case 'image':
+      return (
+        <a href={site} target="_blank" rel="noreferrer">
+          <img className="min-w-40 max-h-full object-cover rounded-3xl drop-shadow-lg border hover:outline-dashed hover:outline-2" src={require(`./Graphics/${media}`)} alt="" />
+        </a>
+      )
+    case 'video':
+      return (
+        <a href={site} target="_blank" rel="noreferrer">
+          <video loop className="min-w-40 max-h-full object-cover rounded-3xl drop-shadow-lg border hover:outline-dashed hover:outline-2" controls autoPlay src={require(`./Graphics/${media}`)} type="video/mp4" />
+        </a>
+      );
+    case 'youtube':
+      return (
+        <div className="youtube-video">
+          <iframe
+            className="min-w-40 max-h-full object-cover rounded-3xl drop-shadow-lg border hover:outline-dashed hover:outline-2"
+            src={media}
+            title="YouTube video"
+            allowFullScreen
+          />
+        </div>
+      );
+
+    default:
+      return null;
+  };
+}
+
+/**
+ * Renders a project component with title, body, GitHub link, site link, collaboration information, technologies used, and media.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title of the project.
+ * @param {string} props.body - The body/content of the project.
+ * @param {string} props.gitHubLink - The GitHub link of the project.
+ * @param {string} props.site - The site link of the project.
+ * @param {string} props.collab - The collaboration information of the project.
+ * @param {string[]} props.techUsed - The technologies used in the project.
+ * @param {string} props.media - The media file name of the project.
+ * @param {string} props.mediaType - The type of media (either 'image' or 'video').
+ * @returns {JSX.Element} The rendered ProjectA component.
+ */
+const ProjectA = ({ title, body, gitHubLink, site, collab, techUsed, media, mediaType }) => {
   return (
     // max-lg:w-3/4 w-11/12
     <div className="h-96 flex w-2/3"> 
       <div className="flex flex-col text-left min-w-60 max-w-70 w-2/3 mr-5">
         <p className="text-6xl font-bold max-xl:text-4xl"> {title} </p>
         <p className="h-auto max-h-300 text-xl max-xl:text-lg"> {body} </p>
-        <p>{collab}</p>
         {techUsed && <div className="grid grid-flow-col auto-cols-max">
-          {techUsed.map((technology) => {
+          {techUsed.map((technology, index) => {
             return (
-              <div class="text-sm lg:text-lg border w-fit p-2 text-white font-bold rounded-2xl bg-red-400">
+              <div key={index} className="text-sm lg:text-lg border h-fit p-2 text-white font-bold rounded-2xl bg-red-400">
                 {technology}
               </div>
             )
@@ -19,15 +64,14 @@ const ProjectA = ({ title, body, image, gitHubLink, site, collab, techUsed, vide
         </div>}
         <div className="h-1/3">
           <a href={gitHubLink} rel="noreferrer" className="" target="_blank" alt="">
-            <button class="border p-2 text-lg text-white font-bold rounded-2xl bg-slate-800 hover:outline-dashed hover:outline-2 outline-black">
+            <button className="border p-2 text-lg text-white font-bold rounded-2xl bg-slate-800 hover:outline-dashed hover:outline-2 outline-black">
               Github
             </button>
           </a>
         </div>
       </div>
       <div className="w-1/3">
-        {image && <a href={site} target="_blank" rel="noreferrer"><img className="min-w-40 max-h-full object-cover rounded-3xl drop-shadow-lg border hover:outline-dashed hover:outline-2" src={require(`./Graphics/${image}`)} alt="" /></a>}
-        {video && <video loop className="min-w-40 max-h-full object-cover rounded-3xl drop-shadow-lg border hover:outline-dashed hover:outline-2" controls autostart autoPlay src={require(`./Graphics/${video}`)} type="video/mp4" />}
+          {media ? renderSwitch(mediaType, media, site) : null}
       </div>
     </div>
   )
--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -2,28 +2,29 @@ import React from 'react'
 import ProjectA from './ProjectA';
 import projectsJson from '../Projects/projects.json'
 const ProjectsList = () => {
-  return (
-		<div className="w-full border-t-4 border-black">
-			<div className="pb-20">
-				<p className="text-8xl font-bold font-sans tracking-tighter text-green-500 drop-shadow"> Projects </p>
-			</div>
-			<div className="flex flex-col w-full items-center">
-				{projectsJson.projects.map((project) => {
-					return <ProjectA
-						title= {project.title}
-						body= {project.body}
-						image= {project.image}
-						video= {project.video}
-						techUsed={project.techUsed}
-						collab={project.collab}
-						gitHubLink= {project.gitHubLink}
-						site= {project.site}
-					/>
-				})}
+	return (
+			<div className="w-full border-t-4 border-black">
+				<div className="pb-20">
+					<p className="text-8xl font-bold font-sans tracking-tighter text-green-500 drop-shadow"> Projects </p>
+				</div>
+				<div className="flex flex-col w-full items-center">
+					{projectsJson.projects.map((project) => {
+						return <ProjectA
+							title= {project.title}
+							body= {project.body}
+							mediaType= {project.mediaType}
+							media= {project.media}
+							techUsed={project.techUsed}
+							collab={project.collab}
+							gitHubLink= {project.gitHubLink}
+							site= {project.site}
+							key= {project.title}
+						/>
+					})}
 
-			</div>
-    </div>
-  )
+				</div>
+		</div>
+	)
 }
 
 export default ProjectsList
