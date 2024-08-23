1b85de62ee232b1f76d9e2b02c61dbf37e70a814
JasonBMa <JasonBMa@users.noreply.github.com>
2024-08-22 23:04:33 -0700

    Made it look floating, full width seems a little too plain and doesn't match the style of projects, so it matches now

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import background from './Graphics/headerBG.svg'
+import Me from './Graphics/Images/Silly.jpg'
 
 const Header = () => {
-  const imageLink = "https://media.licdn.com/dms/image/D5603AQFOIlCi3LDHvQ/profile-displayphoto-shrink_400_400/0/1680680582345?e=1717632000&v=beta&t=TAW-fDZErCa2DIYEPWguJVfMTY1gUY5QrwFdMHmsRXk"
   return (
-    <div id="MainHeader" style={{backgroundImage: `url(${background})`}} className="flex max-lg:flex-col bg-cover w-full h-96 justify-around items-center">
+    <div id="MainHeader" style={{backgroundImage: `url(${background})`}} className="flex max-lg:flex-col bg-cover w-11/12 h-96 justify-center items-center shadow-lg shadow-green-400 m-auto mt-5 rounded">
       <div className="xl:w-1/2 text-7xl max-lg:text-6xl text-start">
         <div className="flex drop-shadow font-bold">
           <p className="drop-shadow font-bold">Hi, I'm</p>
@@ -12,12 +12,13 @@ const Header = () => {
         </div>
         <p className="blur-0">I squash bugs</p>
         <p className="drop-shadow text-4xl max-lg:text-xl text-white">
-          Studying Computer Science @ 
+          BS Computer Science @ 
           <span className="mr-1 bg-gradient-to-r from-blue-600 via-yellow-500 to-yellow-400 inline-block text-transparent bg-clip-text drop-shadow">UC Davis</span>
           </p>
       </div>
-      <div className="w-44 h-44">
-        {imageLink && <img className="object-contain min-w-full min-h-full w-full h-full" src={imageLink}></img>}
+      <div className="w-40 lg:w-64 h-auto">
+        {Me && <img className="h-auto object-contain rounded-tl-3xl rounded-br-3xl" src={Me}>
+        </img>}
       </div>
     </div>
   )
