4ddc34bccfb860993a63eed3d05324e4cdd93435
JasonBMa <JasonBMa@users.noreply.github.com>
2024-08-22 23:03:48 -0700

    changed styling of lists, alterates side of media and text, making it more stylish one could say

--- src/Components/ProjectB.js
+++ src/Components/ProjectB.js
@@ -12,14 +12,14 @@ function Media(mediaType, media, live){
     case 'video':
       content = 
         <a href={live} target="_blank" rel="noreferrer">
-          <video loop className=" shadow-slate-900 object-contain rounded-xl drop-shadow-lg border hover:outline-dashed hover:outline-2" controls autoPlay src={require(`./Graphics/${media}`)} type="video/mp4" />
+          <video loop className="object-contain shadow-slate-900 rounded-xl drop-shadow-lg border hover:outline-dashed hover:outline-2" controls autoPlay src={require(`./Graphics/${media}`)} type="video/mp4" />
         </a>
         break;
     case 'youtube':
       content =
-        <div className="aspect-video">
+        <div className="aspect-video shadow-lg ">
           <iframe
-            className=" shadow-slate-900 w-full h-full rounded-xl drop-shadow-lg border hover:outline-dashed hover:outline-2"
+            className="w-full h-full rounded-xl shadow-slate-900 drop-shadow-lg border hover:outline-dashed hover:outline-2"
             src={media}
             title="YouTube video"
             allowFullScreen
@@ -45,13 +45,16 @@ function Media(mediaType, media, live){
  * @param {string[]} props.techUsed - The technologies used in the project.
  * @param {string} props.media - The media file name of the project.
  * @param {string} props.mediaType - The type of media (either 'image' or 'video').
- * @returns {JSX.Element} The rendered ProjectA component.
+ * @returns {JSX.Element} The rendered ProjectB component.
  */
-const ProjectA = ({ title, body, gitHubLink, live, collab, techUsed, media, mediaType }) => {
+const ProjectB = ({ title, body, gitHubLink, live, collab, techUsed, media, mediaType }) => {
   return (
     // max-lg:w-3/4 w-11/12
-    <div className="h-auto w-full p-1 lg:w-2/3 lg:flex mb-4 justify-center"> 
-      <div className="flex flex-col text-left w-full lg:w-2/4 mr-5">
+    <div className="h-auto w-full p-1 lg:w-2/3 lg:flex mb-4 justify-center">
+      <div className="h-auto w-full lg:w-2/3 mr-5">
+        {media ? Media(mediaType, media, live) : null}
+      </div>
+      <div className="flex flex-col text-left w-full lg:w-2/4">
         <p className="text-6xl font-bold max-xl:text-4xl"> {title} </p>
         <p className="h-auto max-h-300 text-xl max-xl:text-lg"> {body} </p>
         {techUsed && <div className="grid grid-flow-col auto-cols-max">
@@ -70,17 +73,14 @@ const ProjectA = ({ title, body, gitHubLink, live, collab, techUsed, media, medi
             </button>
           </a>
           {live && <a href={live} rel="noreferrer" className="" target="_blank" alt="">
-              <button className="border p-2 text-lg text-white font-bold rounded-2xl bg-slate-800 hover:outline-dashed hover:outline-2 outline-black">
-                Live Demo
-              </button>
+            <button className="border p-2 text-lg text-white font-bold rounded-2xl bg-slate-800 hover:outline-dashed hover:outline-2 outline-black">
+              Live Demo
+            </button>
           </a>}
         </div>
-      </div>
-      <div className="h-auto w-full lg:w-1/3">
-          {media ? Media(mediaType, media, live) : null}
       </div>
     </div>
   )
 }
 
-export default ProjectA
+export default ProjectB
--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -1,27 +1,42 @@
 import React from 'react'
 import ProjectA from './ProjectA';
+import ProjectB from './ProjectB';
 import projectsJson from '../Projects/projects.json'
 const ProjectsList = () => {
+  console.log(projectsJson.projects);
 	return (
-			<div className="w-full border-t-4 border-black">
+			<div className="w-full">
 				<div className="p-5 sm:p-20">
 					<p className="text-4xl sm:text-8xl font-bold font-sans tracking-tighter text-green-500 drop-shadow"> Projects </p>
 				</div>
 				<div className="flex flex-col h-auto w-full items-center">
-					{projectsJson.projects.map((project) => {
-						return <ProjectA
-							title= {project.title}
-							body= {project.body}
-							mediaType= {project.mediaType}
-							media= {project.media}
-							techUsed={project.techUsed}
-							collab={project.collab}
-							gitHubLink= {project.gitHubLink}
-							site= {project.site}
-							key= {project.title}
-						/>
+					{projectsJson.projects.map(function(project,index) {
+            if (index % 2 === 0) {
+              return <ProjectA
+                title= {project.title}
+                body= {project.body}
+                mediaType= {project.mediaType}
+                media= {project.media}
+                techUsed= {project.techUsed}
+                collab= {project.collab}
+                gitHubLink= {project.gitHubLink}
+                live= {project.live}
+                key= {project.title}
+              />
+            } else {
+              return <ProjectB
+                title= {project.title}
+                body= {project.body}
+                mediaType= {project.mediaType}
+                media= {project.media}
+                techUsed= {project.techUsed}
+                collab= {project.collab}
+                gitHubLink= {project.gitHubLink}
+                live= {project.live}
+                key= {project.title}
+              />
+            }
 					})}
-
 				</div>
 		</div>
 	)
